90ba5b9974acb1311bec8ea8116f56e4e5f155c9
Jeonghoon-Lee <hoon22.lee@gmail.com>
2019-01-11 20:51:41 -0500

    Add comment and fix bugs

--- javascript/assignment1/1896357.js
+++ javascript/assignment1/1896357.js
@@ -1,4 +1,12 @@
-var studentList = [
+/**
+ * 420-PX4-AB JAVASCRIPT Assignment-1
+ * 
+ *  - Author: Jeonghoon Lee(1896357)
+ *  - Date: Jan. 11, 2019
+ */
+
+// Array of students' name
+let students = [
     { firstName: 'Aakash', lastName: 'Malhotra' },
     { firstName: 'Atreya Paudel', lastName: 'Vijit' },
     { firstName: 'Tyler', lastName: 'Beland' },
@@ -25,8 +33,9 @@ var studentList = [
 
 
 /**
- * Return string of full name
- * @param {string} student 
+ * Return string of fullname
+ * @param {object} student 
+ * @return {string} fullname of student
  */
 function getFullName(student) {
     return student.firstName + ' ' + student.lastName;
@@ -34,15 +43,15 @@ function getFullName(student) {
 
 
 /**
- * Search a student by name and return index in an array
+ * Search a student by name and return array of indexes
  * @param {string} firstName 
  * @param {string} lastName
- * @return {array of number} array of matched index of student name in the array
+ * @return {array of number} array of matched index of student name
  */
 function searchByName(firstName, lastName) {
     let mactchedIndexes = [];
 
-    studentList.forEach(function(student, index) {
+    students.forEach(function(student, index) {
         if (student.firstName == firstName && student.lastName == lastName) {
             mactchedIndexes.push(index);
         }
@@ -52,20 +61,24 @@ function searchByName(firstName, lastName) {
 
 
 /**
- * Add a new student
+ * Add a new student into the array of students
  * @param {string} firstName 
  * @param {string} lastName 
+ * @return {number} length of students array
  */
 function addStudent(firstName, lastName) {
-    studentList.push({firstName, lastName});
+    students.push({firstName, lastName});
+    return students.length;
 }
 
 
 /**
- * Remove a student from the array
+ * Remove a student at index from the students array
+ * @param {number} index
+ * @return {object} deleted student object
  */
-function removeStudent() {
-    studentList.pop();
+function removeStudent(index) {
+    return students.splice(index, 1)[0];
 }
 
 
@@ -73,57 +86,56 @@ function removeStudent() {
  * Print all student name in the array
  */
 function printAll() {
-    studentList.forEach(function(student) {
+    console.log('\n------- student list -------\n')
+    students.forEach(function(student, index) {
         console.log(getFullName(student));
     });
 }
 
 
 /**
- * 
+ * Compare student fullname for ascending order without case sensitivity
  * @param {object} student1 
  * @param {object} student2 
- * @return {number} A negative number if student1 occurs before student2; 
- *                  positive if student1 occurs after student2;
- *                  0 if they are equivalent.
+ * @return {number} A negative number if student1 occurs before student2;
+ *     positive if student1 occurs after student2; 0 if they are equivalent.
  */
 function compareNameByAscending(student1, student2) {
-    let fullname1 = getFullName(student1).toLowerCase();
-    let fullname2 = getFullName(student2).toLowerCase();
+    let fullName1 = getFullName(student1).toLowerCase();
+    let fullName2 = getFullName(student2).toLowerCase();
 
-    return fullname1.localeCompare(fullname2);
+    return fullName1.localeCompare(fullName2);
 }
 
 
 /**
- * 
+ * Compare student fullname for descending order without case sensitivity
  * @param {object} student1 
  * @param {object} student2 
- * @return {number} A negative number if student2 occurs before student1; 
- *                  positive if student2 occurs after student1;
- *                  0 if they are equivalent.
+ * @return {number} A negative number if student2 occurs before student1;
+ *     positive if student2 occurs after student1; 0 if they are equivalent.
  */
 function compareNameByDescending(student1, student2) {
-    let fullname1 = getFullName(student1).toLowerCase();
-    let fullname2 = getFullName(student2).toLowerCase();
+    const fullName1 = getFullName(student1).toLowerCase();
+    const fullName2 = getFullName(student2).toLowerCase();
 
-    return fullname2.localeCompare(fullname1);
+    return fullName2.localeCompare(fullName1);
 }
 
 
 /**
- * Sort student array by name and print
- * @param {boolean} ascendingOrder
+ * Sort student array by name and print all
+ *  - if ascendingOrder is true, then sort array in ascending order
+ *  - else sort array in descending order
+ * @param {boolean} ascendingOrder 
  */
 function printSortedList(ascendingOrder) {
     let sortedArray = [];
     
-    if (ascendingOrder) {
-        sortedArray = studentList.sort(compareNameByAscending);
-    } else {
-        sortedArray = studentList.sort(compareNameByDescending);
-    }
+    sortedArray = (ascendingOrder ? students.sort(compareNameByAscending) : students.sort(compareNameByDescending));
+
     // print sorted student list
+    console.log('\n------- sorted student list -------\n')
     sortedArray.forEach(student => console.log(getFullName(student)));
 }
 
@@ -132,28 +144,27 @@ function printSortedList(ascendingOrder) {
  * Print random student name
  */
 function printRandomName() {
-    let randomIndex = Math.floor(Math.random() * studentList.length);
+    const randomIndex = Math.floor(Math.random() * students.length);
 
-    console.log(getFullName(studentList[randomIndex]));
+    console.log(getFullName(students[randomIndex]));
 }
 
 
 /**
- * Take the third letter of every student's name, 
- * append it to the beginning of their name 
+ * Take the third letter of every student's name, append it to the beginning of their name 
  * and display the sorted list of students 
  */
 function sortNameByThirdLetter() {
     let newStudentArray = [];
 
-    // Make new student name array 
-    // that append the third letter of name on beginning of name
-    newStudentArray = studentList.map(function(student) {
-        let fullName = getFullName(student);
+    // Make new student name array that append the third letter of name on the beginning
+    newStudentArray = students.map(function(student) {
+        const fullName = getFullName(student);
         return fullName[2] + fullName;
     });
 
     // Sort and print result
+    console.log('\n------- sorted student list -------\n')
     newStudentArray.sort().forEach(name => console.log(name));
 }
 
@@ -165,15 +176,16 @@ switch (functionToOperator) {
         console.log(searchByName('Jeonghoon', 'Lee'));
         break;
     case 2:
-        addStudent('James', 'Dean');
+        console.log('length of array: ' + addStudent('James', 'Dean'));
         break;
     case 3:
-        removeStudent();
+        console.log(removeStudent(0));
         break;
     case 4:
         printAll();
         break;
     case 5:
+        // sort array in ascending order
         printSortedList(true);
         break;
     case 6:
