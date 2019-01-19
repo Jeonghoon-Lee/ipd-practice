da90e8e10c080c79176ba070e9c4afe2a47c92f0
Jeonghoon-Lee <hoon22.lee@gmail.com>
2019-01-18 22:51:44 -0500

    Add jquery api and ajax call Add error handling and href link Modify html tables

--- javascript/project/login/js/login.js
+++ javascript/project/login/js/login.js
@@ -9,9 +9,9 @@
  * Display login error message.
  * @param {string} errorMsg error message you want to display
  */
-function displayErrorMsg(errorMsg) {  
+function displayErrorMsg(errorMsg) {
     document.querySelector('#error-header').innerHTML = 'Please complete the form!';
-    
+
     // Insert error message into HTML list
     const errorMessageItem = document.createElement('li');
     errorMessageItem.innerHTML = errorMsg;
@@ -68,7 +68,7 @@ function clearErrorMsg() {
 
     // Remove list items of error message
     const errorlist = document.querySelector('#error-message');
-    while(errorlist.firstChild != null) {
+    while (errorlist.firstChild != null) {
         errorlist.removeChild(errorlist.firstChild);
     }
 }
@@ -92,39 +92,83 @@ function displayWelcomeMsg() {
 }
 
 /**
- * Start login process.
+ * Make weather information table
+ * @param {object} weatherForecast converted from accuweather json data
  */
-function login() {
-    // Get an unique API key from https://developer.accuweather.com/packages
-    const MY_KEY = 'InsertYourKey';
-    const weatherURL = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/56186?apikey=' + MY_KEY + '&metric=true';
-
-    // Create AJAX request
-    const request = new XMLHttpRequest();
-    request.open('GET', weatherURL);
-    // Callback function for response
-    request.onload = function (e) {
-        const weatherForecast = JSON.parse(request.response);   // parsing response
-
-        // Get weather table for display
-        const tableRows = document.querySelectorAll('tbody tr');
-        // Add new data for each days
-        weatherForecast.DailyForecasts.forEach(function (dailyReport, index) {
-            const options = { weekday: 'short' };
-            tableRows[index].querySelector('th').innerHTML = new Date(dailyReport.Date).toLocaleDateString('en-US', options);
-    
-            const tableData = tableRows[index].querySelectorAll('td');
-            // Set actual data from JavaScript object
-            tableData[0].innerHTML = dailyReport.Temperature.Maximum.Value + '&#x2103;<br>' + dailyReport.Temperature.Minimum.Value + '&#x2103;';
-            tableData[1].innerHTML = dailyReport.Day.IconPhrase;
-            tableData[2].innerHTML = dailyReport.Night.IconPhrase
-        });
-        // Make weather container visible
-        document.querySelector('.weather-container').style.display = 'block';
+function displayWeather(weatherForecast) {
+    // Get weather table for display
+    const tableBody = document.querySelector('tbody');
+    // Remove all the child elements if exist
+    while (tableBody.firstChild != null) {
+        tableBody.removeChild(tableBody.firstChild);
     }
-    request.send();
+    // Add new data for each days
+    weatherForecast.DailyForecasts.forEach(function (dailyReport) {
+        // create table row
+        let tableRow = document.createElement('tr');
+        // set header element
+        let tableHeader = document.createElement('th');
+        const options = { weekday: 'short' };
+        tableHeader.innerHTML = new Date(dailyReport.Date).toLocaleDateString('en-US', options);
+        tableHeader.scope = 'row';
+        // create table data element
+        const tableData1 = document.createElement('td');
+        const tableData2 = document.createElement('td');
+        const tableData3 = document.createElement('td');
+
+        // Set actual data from JavaScript object
+        tableData1.innerHTML = dailyReport.Temperature.Maximum.Value + '&#x2103;<br>' + dailyReport.Temperature.Minimum.Value + '&#x2103;';
+        tableData2.innerHTML = dailyReport.Day.IconPhrase;
+        tableData3.innerHTML = dailyReport.Night.IconPhrase
+
+        // Add link to accuweather website
+        tableRow.onclick = (() => parent.open(dailyReport.Link));
+        // Append row into table
+        tableRow.appendChild(tableHeader);
+        tableRow.appendChild(tableData1);
+        tableRow.appendChild(tableData2);
+        tableRow.appendChild(tableData3);
+        tableBody.appendChild(tableRow);
+    });
+    // Make weather container visible
+    document.querySelector('.weather-container').style.display = 'block';
 }
 
+/**
+ * Make data request to accuweather and handle received data.
+ */
+function getWeatherInfo() {
+    // Get an unique API key from https://developer.accuweather.com/packages
+    const MY_KEY = 'kBPth39Aqwu6hqfNdRPFngYwdR1LMZXv';
+    const weatherURL = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/56186?apikey=' + MY_KEY + '&metric=true';
+
+    /**
+     * Using jquery
+     */
+    $.ajax({
+        url: weatherURL,
+    }).done(function (weatherForecast) {
+        displayWeather(weatherForecast);
+    }).fail(function (jqXHR) {
+        console.error(jqXHR);
+    });
+
+    /**
+     * Using XHR
+     */
+    // const request = new XMLHttpRequest();
+    // request.open('GET', weatherURL);
+    // // Callback function for response
+    // request.onload = function(e) {
+    //     const weatherForecast = JSON.parse(request.response);   // parsing response
+    //     displayWeather(weatherForecast)
+    //  }
+    // // error handling
+    // request.onerror = function(e) {
+    //     console.error("[Error] Request was failed.");
+    // }
+    // request.send();
+}
 
 // login button click event handler
 document.querySelector('#login-button').onclick = function () {
@@ -145,7 +189,7 @@ document.querySelector('#login-button').onclick = function () {
             // clear input password field after login
             document.querySelector('[type="password"]').value = '';
             displayWelcomeMsg();
-            login();
+            getWeatherInfo();
         }
     } else {    // logout process
         // Remove welcome message container from HTML
@@ -158,34 +202,83 @@ document.querySelector('#login-button').onclick = function () {
     }
 }
 
-
-// Load initial status
-//  - today's message, date and time
-const todayMsgContainer = document.querySelector('#today-message');
+// Display date and time
+const timeContainer = document.querySelector('#time-container');
 const today = new Date();
 
-// Date string option setting
-const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+// Create span element to display date and time
+const dateSpan = document.createElement('span');
+const timeSpan = document.createElement('span');
 
-document.querySelector('#today').innerHTML = today.toLocaleDateString('en-US', options);
-document.querySelector('#current-time').innerHTML = today.toLocaleTimeString('en-US');
+dateSpan.innerHTML = today.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' });
+dateSpan.classList.add('mr-2');
+timeSpan.innerHTML = today.toLocaleTimeString('en-US');
+
+timeContainer.appendChild(dateSpan);
+timeContainer.appendChild(timeSpan);
 
 // Set interval to display clock.
-window.setInterval(function(){
+window.setInterval(function () {
     const today = new Date();
-    document.querySelector('#current-time').innerHTML = today.toLocaleTimeString('en-US');
+    document.querySelectorAll('#time-container span')[1].innerHTML = today.toLocaleTimeString('en-US');
 }, 1000);
+
+
+/**
+ * Create HTML element for famous quotes and display it.
+ * @param {object} famousQuotes converted from json
+ */
+function displayFamousQuotes(famousQuotes) {
+    const quoteElement = document.createElement('p');
+    const authorElement = document.createElement('footer')
+
+    quoteElement.innerHTML = '"' + famousQuotes[0].quote + '"';
+    authorElement.innerHTML = '<strong>' + famousQuotes[0].author + '</strong>';
+
+    quoteElement.classList.add('mb-0', 'text-info');
+    authorElement.classList.add('blockquote-footer');
+
+    document.querySelector('blockquote').appendChild(quoteElement);
+    document.querySelector('blockquote').appendChild(authorElement);
+    document.querySelector('blockquote').style.display = 'block';
+}
 
 // Famous Quotes URL
 const famousQuotesURL = 'https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=famous&count=1';
+
+/**
+ * Using jquery
+ */
+$.ajax({
+    url: famousQuotesURL,
+    headers: {
+        'X-RapidAPI-Key': '577a1fcbf6msh8c12c140bf08a60p124934jsn2387f29acd2f'
+    }
+}).done(function (famousQuotes) {
+    displayFamousQuotes(famousQuotes);
+}).fail(function () {
+    // Display default Quotes
+    displayFamousQuotes([{quote:'If you want to be happy, be.', author:'Leo Tolstoy'}]);
+    // Print error to console
+    console.error('Error! from famousQuote');
+});
+
+/**
+ * Using XHR
+ */
 // Create AJAX request
-const quotesRequest = new XMLHttpRequest();
-quotesRequest.open('GET', famousQuotesURL);
-quotesRequest.setRequestHeader('X-RapidAPI-Key', '577a1fcbf6msh8c12c140bf08a60p124934jsn2387f29acd2f');
-// Callback function for response
-quotesRequest.onload = function (e) {
-    const famousQuotes = JSON.parse(quotesRequest.response);   // parsing response
-    document.querySelector('blockquote p').innerHTML = '"' + famousQuotes[0].quote + '"';
-    document.querySelector('blockquote footer').innerHTML = '<strong>' + famousQuotes[0].author + '</strong>';
-};
-quotesRequest.send();
+// const quotesRequest = new XMLHttpRequest();
+// quotesRequest.open('GET', famousQuotesURL);
+// quotesRequest.setRequestHeader('X-RapidAPI-Key', '577a1fcbf6msh8c12c140bf08a60p124934jsn2387f29acd2f');
+
+// // Callback function for response
+// quotesRequest.onload = function (e) {
+//     const famousQuotes = JSON.parse(quotesRequest.response);   // Convert response to Java object
+//     displayFamousQuotes(famousQuotes);
+// };
+
+// // Error handling
+// quotesRequest.onerror = function(e) {
+//     console.error("XMLHttpRequest() failed.");
+// }
+// quotesRequest.send();
